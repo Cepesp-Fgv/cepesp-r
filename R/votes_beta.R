7e6b52a4a8c76444c09f0b86374ab10e90b67b15
Luiz Gabriel <luizgabriel.info@gmail.com>
2017-12-07 11:29:41 -0300

    Fixed votes_x_candidates function

--- R/votes_beta.R
+++ R/votes_beta.R
@@ -1,5 +1,5 @@
 votes_x_candidates <- function (year=2014, regional_aggregation="Municipality", position="President", cached=FALSE, columns_list=list(), state="AC", party=NULL, candidate_number=NULL) {
-  candidates <- candidates(year, position, cached, list(), state, party, candidate_number)
+  candidates <- candidates(year, position, cached, list(), party, candidate_number)
   votes <- votes(year, regional_aggregation, position, cached, list(), party, candidate_number)
 
   result <- merge(votes, candidates, by="NUMERO_CANDIDATO", all.x = TRUE)
